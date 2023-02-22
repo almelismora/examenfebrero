dd53522707860da6a8543b4181eb4e212eb88f77
Elis Mora <almedinaelis.mora@gmail.com>
2023-02-22 09:51:34 +0100

    vistas simples de home page y fotos creadas

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const fotosRouter = require('./routes/fotos');
 
 const app = express();
 
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/fotos', fotosRouter);
 
 app.use(express.static(path.join(__dirname, 'public')));
 // catch 404 and forward to error handler
--- routes/fotos.js
+++ routes/fotos.js
@@ -3,7 +3,9 @@ const router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('fotos');
 });
+
+
 
 module.exports = router;
